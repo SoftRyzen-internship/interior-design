18d3623e29ea1f6d8a878061ed3e6c5475432104
egarra <garikhabuzov@gmail.com>
2023-02-07 17:06:39 +0200

    FE-8: swiper create with adaptation

---
+++ src/assets/js/swiperOurTeam.js
@@ -0,0 +1,22 @@
+import Swiper, { Navigation } from 'swiper';
+
+Swiper.use(Navigation)
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    grabCursor: true,
+    slidesPerView: 2,
+    spaceBetween: 28,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        1440: {
+            slidesPerView: 3,
+            spaceBetween: 35,
+        }
+    }
+  });
