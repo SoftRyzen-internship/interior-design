47a9baa59400ef85f6c63bcf6c4526d4a8a6a0bf
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2023-02-08 23:43:31 +0200

    FE-9: Add validation and styles on input

--- src/assets/js/form.js
+++ src/assets/js/form.js
@@ -1,62 +1,80 @@
-const form = document.querySelector('form');
+const form = document.querySelector('.form');
 
+const nameField = document.getElementById('name');
+const phoneField = document.getElementById('phone');
+const messageField = document.getElementById('message');
+
+// const nameLabel = document.querySelector('.form__label--name');
+
+form.addEventListener('input', onInputChange);
 form.addEventListener('submit', onHandleSubmit);
 
-function onHandleSubmit(event) {
-  event.preventDefault();
-
-  const {
-    elements: { name, phone, message },
-  } = event.currentTarget;
-
-  const result = { name: name.value, phone: phone.value, message: message.value };
-  event.currentTarget.reset();
+function onInputChange({ target }) {
+  if (target.hasAttribute('data-reg')) {
+    inputCheck(target);
+  }
 }
 
-const mask = selector => {
-  let setCursorPosition = (pos, elem) => {
-    elem.focus();
+function inputCheck(el) {
+  let isValid = true;
+  const inputValue = el.value.trim();
+  const inputReg = el.getAttribute('data-reg');
+  const reg = new RegExp(inputReg);
+  if (reg.test(inputValue)) {
+    setSuccess(el);
+  } else {
+    setError(el);
+    isValid = false;
+  }
+  return isValid;
+}
 
-    if (elem.setSelectionRange) {
-      elem.setSelectionRange(pos, pos);
-    } else if (elem.createTextRange) {
-      let range = elem.createTextRange();
+function onHandleSubmit(e) {
+  e.preventDefault();
+  let isFormNotEmpty = checkEmptyField(nameField, phoneField, messageField);
+  let isNameValid = inputCheck(nameField);
+  let isPhoneValid = inputCheck(phoneField);
+  let isFormValid = isFormNotEmpty && isNameValid && isPhoneValid;
+  if (isFormValid) {
+    e.currentTarget.reset();
+  }
+}
 
-      range.collapse = true;
-      range.moveEnd('character', pos);
-      range.moveStart('character', pos);
-      range.select();
-    }
-  };
-
-  function createMask(event) {
-    let matrix = '+38 (___) ___ __ __ ',
-      i = 0,
-      def = matrix.replace(/\D/g, ''),
-      val = this.value.replace(/\D/g, '');
-
-    if (def.length >= val.length) {
-      val = def;
-    }
-
-    this.value = matrix.replace(/./g, a => {
-      return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
-    });
-
-    if (event.type === 'blur') {
-      if (this.value.length == 2) {
-        this.value = '';
-      }
-    } else {
-      setCursorPosition(this.value.length, this);
-    }
+function checkEmptyField(name, phone, message) {
+  let isValid = true;
+  if (name.value.trim() === '') {
+    setError(name);
+    isValid = false;
+  } else {
+    setSuccess(name);
   }
 
-  let input = document.querySelector(selector);
+  if (phone.value.trim() === '') {
+    setError(phone);
+    isValid = false;
+  } else {
+    setSuccess(phone);
+  }
 
-  input.addEventListener('input', createMask);
-  input.addEventListener('blur', createMask);
-  input.addEventListener('focus', createMask);
-};
+  if (message.value.trim() === '') {
+    setError(message);
+    isValid = false;
+  } else {
+    setSuccess(message);
+  }
+  return isValid;
+}
 
-mask('[name="phone"]');
+function setError(field) {
+  field.classList.add('error__input');
+  field.classList.remove('success__input');
+  // nameLabel.classList.add('error__label');
+  // nameLabel.classList.remove('success__label');
+}
+
+function setSuccess(field) {
+  field.classList.add('success__input');
+  field.classList.remove('error__input');
+  // nameLabel.classList.add('success__label');
+  // nameLabel.classList.remove('error__label');
+}
