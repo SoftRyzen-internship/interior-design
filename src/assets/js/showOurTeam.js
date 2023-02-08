2c6f95f27d48f17070d515232a4bf352c2286b20
egarra <garikhabuzov@gmail.com>
2023-02-08 12:56:04 +0200

    FE-8: fix mobile logic

--- src/assets/js/showOurTeam.js
+++ src/assets/js/showOurTeam.js
@@ -1,21 +1,27 @@
 
 const showOurTeam = () => {
-    const btn = document.querySelector('.team__btn');
-          cards = document.querySelectorAll('.team__list-item');
+    const secionList = document.querySelector('.team__list-wrapper');
+    const btn = secionList.querySelector('.team__btn');
+    const cards = secionList.querySelectorAll('.team__list-item');
 
-    btn.addEventListener('click', () => {
-        cards.forEach(card => {
-            if (card.classList.contains('team__is-hidden')) {
-                card.classList.remove('team__is-hidden')
+    secionList.addEventListener('click', (e) => {
+        
+        if (e.target.closest('.team__btn')) {
 
-                window.scrollBy({
-                    top: 300,
-                    behavior: "smooth",
-                    });
-
-                    btn.classList.add('team__is-hidden')
-            }
-        })
+            cards.forEach(card => {
+                if (card.classList.contains('team__is-hidden')) {
+                    card.classList.remove('team__is-hidden')
+    
+                    window.scrollBy({
+                        top: 300,
+                        behavior: "smooth",
+                        });
+    
+                        btn.classList.add('team__is-hidden')
+                }
+            })
+        }
+        
     })
 }
 showOurTeam()
