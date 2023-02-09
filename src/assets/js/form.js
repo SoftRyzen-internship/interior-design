ff44113adb1860a274f580538cbb51fc74acd10d
Kuznietsova Oksana <kuznietsova.ok@gmail.com>
2023-02-09 14:37:13 +0200

    FE-9: Fix logic, add template for errors

--- src/assets/js/form.js
+++ src/assets/js/form.js
@@ -4,10 +4,14 @@ const nameField = document.getElementById('name');
 const phoneField = document.getElementById('phone');
 const messageField = document.getElementById('message');
 
-// const nameLabel = document.querySelector('.form__label--name');
+const errorMessage = document.querySelectorAll('.form__error');
+
+const button = document.querySelector('.form__button');
 
 form.addEventListener('input', onInputChange);
 form.addEventListener('submit', onHandleSubmit);
+phoneField.addEventListener('blur', onInputChange);
+button.addEventListener('click', onButtonClick);
 
 function onInputChange({ target }) {
   if (target.hasAttribute('data-reg')) {
@@ -18,8 +22,10 @@ function onInputChange({ target }) {
 function inputCheck(el) {
   let isValid = true;
   const inputValue = el.value.trim();
+
   const inputReg = el.getAttribute('data-reg');
   const reg = new RegExp(inputReg);
+
   if (reg.test(inputValue)) {
     setSuccess(el);
   } else {
@@ -31,50 +37,24 @@ function inputCheck(el) {
 
 function onHandleSubmit(e) {
   e.preventDefault();
-  let isFormNotEmpty = checkEmptyField(nameField, phoneField, messageField);
   let isNameValid = inputCheck(nameField);
   let isPhoneValid = inputCheck(phoneField);
-  let isFormValid = isFormNotEmpty && isNameValid && isPhoneValid;
+  let isFormValid = isNameValid && isPhoneValid;
   if (isFormValid) {
     e.currentTarget.reset();
   }
 }
 
-function checkEmptyField(name, phone, message) {
-  let isValid = true;
-  if (name.value.trim() === '') {
-    setError(name);
-    isValid = false;
-  } else {
-    setSuccess(name);
-  }
-
-  if (phone.value.trim() === '') {
-    setError(phone);
-    isValid = false;
-  } else {
-    setSuccess(phone);
-  }
-
-  if (message.value.trim() === '') {
-    setError(message);
-    isValid = false;
-  } else {
-    setSuccess(message);
-  }
-  return isValid;
-}
-
 function setError(field) {
   field.classList.add('error__input');
   field.classList.remove('success__input');
-  // nameLabel.classList.add('error__label');
-  // nameLabel.classList.remove('success__label');
 }
 
 function setSuccess(field) {
   field.classList.add('success__input');
   field.classList.remove('error__input');
-  // nameLabel.classList.add('success__label');
-  // nameLabel.classList.remove('error__label');
+}
+
+function onButtonClick(event) {
+  event.preventDefault();
 }
