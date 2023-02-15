e83a9dfd97921bca8841fce67d752d340f9a2078
GubarievaAnna <annagubarieva@gmail.com>
2023-02-15 09:19:05 +0200

    FE-1: Fix modal animation

--- src/assets/js/slider.js
+++ src/assets/js/slider.js
@@ -1,12 +1,12 @@
 $('.slider').slick({
   arrows: false,
   dots: true,
-  speed: 1000,
+  speed: 300,
   cssEase: 'ease-in-out',
 });
 
 $('.sliderModal').slick({
-  speed: 1000,
+  speed: 300,
   cssEase: 'ease-in-out',
   variableWidth: true,
 });
